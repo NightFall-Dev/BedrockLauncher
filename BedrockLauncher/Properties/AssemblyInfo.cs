5f0adbc9d9a0da9287517d18ad24f8520d5d09a9
MetalManeMc <metalmaniac1331@outlook.com>
2025-06-17 23:45:36 +0200

    Finished page resources

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2025.6.17.94")]
+[assembly: AssemblyVersion("2025.6.17.105")]
 [assembly: NeutralResourcesLanguage("en-US")]
