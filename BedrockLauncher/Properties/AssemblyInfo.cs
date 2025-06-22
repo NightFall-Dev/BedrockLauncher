a4aba241b910969325eea444684308a0d0db13fa
MetalManeMc <metalmaniac1331@outlook.com>
2025-06-22 14:38:38 +0200

    Package launching backup
    When URI launch fails, Launcher attempts to launch package directly.
    My URIs work so I can not test if this actually launcher

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2025.6.20.4")]
+[assembly: AssemblyVersion("2025.6.22.8")]
 [assembly: NeutralResourcesLanguage("en-US")]
