f935909afede44b6d708fb9ee55927119a886dc1
Ryan Morrison <Rayth@users.noreply.github.com>
2025-07-13 22:05:28 +0100

    Emergency Bug Fix
    Wasn't working for people with .NET 8

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2025.7.13.10")]
+[assembly: AssemblyVersion("2025.7.13.17")]
 [assembly: NeutralResourcesLanguage("en-US")]
